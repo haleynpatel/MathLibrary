4de93a174b879725b656da264e91b6e3b9950803
Haleynp <haleynpatel@gmail.com>
2024-01-05 14:50:52 -0500

    Rename file

---
+++ include/arithmetic_operations.h
@@ -0,0 +1,11 @@
+//arithmetic operations
+#pragma once
+#include <stdexcept>
+
+class MathOperations {
+public:
+    static int add(int a, int b);
+    static int subtract(int a, int b);
+    static int multiply(int a, int b);
+    static int divide(int a, int b);
+};
