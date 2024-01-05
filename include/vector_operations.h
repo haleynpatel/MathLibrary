d3eaa5b97c2a8f0cdb81ba2d87bbede31d36be43
Haleynp <haleynpatel@gmail.com>
2024-01-05 15:00:46 -0500

    Update method names to be more specific

--- include/vector_operations.h
+++ include/vector_operations.h
@@ -5,11 +5,11 @@
 
 class VectorOperations {
 public:
-    static std::vector<int> add(const std::vector<int>& a, const std::vector<int>& b);
-    static std::vector<int> multiply(const std::vector<int>& a, int scalar);
-    static std::vector<int> subtract(const std::vector<int>& a, const std::vector<int>& b);
-    static std::vector<int> crossProduct(const std::vector<int>& a, const std::vector<int>& b);
-    static int dotProduct(const std::vector<int>& a, const std::vector<int>& b);
-    static double magnitude(const std::vector<int>& a);
-    
+    static std::vector<int> vectorAddition(const std::vector<int>& a, const std::vector<int>& b);
+    static std::vector<int> vectorMultiply(const std::vector<int>& a, int scalar);
+    static std::vector<int> vectorSubtract(const std::vector<int>& a, const std::vector<int>& b);
+    static std::vector<int> vectorCrossProduct(const std::vector<int>& a, const std::vector<int>& b);
+    static int vectorDotProduct(const std::vector<int>& a, const std::vector<int>& b);
+    static double vectorMagnitude(const std::vector<int>& a);
+
 };
