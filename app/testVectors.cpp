801a3fc75bb008a08d972fc6bff40ccf8c91a821
Haleynp <haleynpatel@gmail.com>
2024-01-08 17:37:44 -0500

    Test scalar multiplication

--- app/testVectors.cpp
+++ app/testVectors.cpp
@@ -7,7 +7,7 @@ int main() {
     // Vectors
     std::vector<int> vector_a = {1, 2, 3};
     std::vector<int> vector_b = {4, 5, 6};
-
+    int scalar = 5;
     // Vector Addition
     std::vector<int> result_vector_add = VectorOperations::vectorAddition(vector_a, vector_b);
     std::cout << "Vector Addition Result: ";
@@ -17,6 +17,11 @@ int main() {
     std::vector<int> result_vector_subtract = VectorOperations::vectorSubtract(vector_a, vector_b);
     std::cout << "Vector Subtraction Result: ";
     VectorOperations::printVector(result_vector_subtract);
+
+    // Vector Scalar Multiplication with Scalar
+    std::vector<int> result_scalar = VectorOperations::vectorMultiply(vector_a, scalar);
+    std::cout << "Vector Scalar Multiplication Result: ";
+    VectorOperations::printVector(result_scalar);
 
     return 0;
 }
