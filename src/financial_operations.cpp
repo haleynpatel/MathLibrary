d04d180bddcd27e989a3f05ebfdd39653bea2373
Haleynp <haleynpatel@gmail.com>
2024-01-17 23:27:52 -0500

    Implement future value calculation method

--- src/financial_operations.cpp
+++ src/financial_operations.cpp
@@ -4,3 +4,7 @@
 double FinancialOperations::calculatePresentValue(double futureValue, double discountRate, int periods) {
     return futureValue / pow(1 + discountRate, periods);
 }
+
+double FinancialOperations::calculateFutureValue(double presentValue, double discountRate, int periods) {
+    return presentValue * pow(1 + discountRate, periods);
+}
