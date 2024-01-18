bb515fa115f1a55cb57eef37e01c6e6c19cd410a
Haleynp <haleynpatel@gmail.com>
2024-01-17 23:08:19 -0500

    Initial commit

---
+++ include/financials.h
@@ -0,0 +1,9 @@
+// Financial Calculators
+#pragma once
+
+#include <cmath>
+
+class FinancialOperations {
+public:
+    static double presentValue(double futureValue, double discountRate, int periods);
+};
